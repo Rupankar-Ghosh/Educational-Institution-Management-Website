faa17e2f734af720053247ee6baba4e8b9032a28
Rupankar Ghosh <rupankarghosh2012@gmail.com>
2025-02-09 18:09:09 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,19 @@
-js
+   console.log(2);
+        console.error("Error");
+        console.warn("Warning");
+
+        s=document.querySelector("#a");
+        console.log(s);
+        s.style.background="black";
+        s.style.color="rgb(255, 213, 0)";
+
+        function clickjs(a){
+            s=document.querySelector(a);
+            s.style.color="white";
+        }
+
+        a.addEventListener("click", function(){console.log("c");})
+        a.addEventListener("mouseover", function(){console.log("toutch");})
+
+
+    
